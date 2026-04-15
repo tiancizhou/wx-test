24f37b86734e2eb7f22dcbaa84cfcf1706426533
Bob <1294811773@qq.com>
2026-04-15 18:47:37 +0800

    config: 填入微信服务号配置
    APP_ID / APP_SECRET / TOKEN / ENCODING_AES_KEY 已配置

--- backend/wechat/config.py
+++ backend/wechat/config.py
@@ -3,9 +3,9 @@ import os
 
 class WeChatSettings:
     TOKEN: str = os.getenv("WX_TOKEN", "qq5201314")
-    APP_ID: str = os.getenv("WX_APP_ID", "")
-    APP_SECRET: str = os.getenv("WX_APP_SECRET", "")
-    ENCODING_AES_KEY: str = os.getenv("WX_ENCODING_AES_KEY", "")
+    APP_ID: str = os.getenv("WX_APP_ID", "wx418d84117fe16bcc")
+    APP_SECRET: str = os.getenv("WX_APP_SECRET", "6bfb1b36386d11e4efba9d2fd2ef38f9")
+    ENCODING_AES_KEY: str = os.getenv("WX_ENCODING_AES_KEY", "wkTzbshp2Plx5QZ0uQVcKizai5F1ZCoEARuochQUAkQ")
 
 
 settings = WeChatSettings()
